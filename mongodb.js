241c451ad39901aacf47821a8f30715a640ebb8b
sbp8984 <87753278+sbp8984@users.noreply.github.com>
2021-09-21 21:18:50 +0100

    updated mongodb -v

--- mongodb.js
+++ mongodb.js
@@ -5,5 +5,17 @@ const MongoClient = mongodb.MongoClient
 
 //connection
 const connectionURL = 'mongodb://127.0.0.1:27017'
-const database = 'task-manager'
+const databaseName = 'task-manager'
 
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error,client) => {
+    if (error) {
+        return console.log('unable to connect to db')
+    } 
+
+    console.log('connected to db')
+    // const db = client.db(databaseName)
+
+    // db.collection('users').insertOne({
+        
+    // })
+})
