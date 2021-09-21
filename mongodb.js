bfa84773e73ab74b86ce39600512815347ce5609
sbp8984 <87753278+sbp8984@users.noreply.github.com>
2021-09-21 22:12:27 +0100

    insert many function added

--- mongodb.js
+++ mongodb.js
@@ -12,10 +12,58 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error,client) => 
         return console.log('unable to connect to db')
     } 
 
-    console.log('connected to db')
-    // const db = client.db(databaseName)
+    const db = client.db(databaseName)
 
     // db.collection('users').insertOne({
-        
+    //     name: 'Shyam',
+    //     age: 30
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('unable to insert user')
+    //     } 
+
+    //     console.log(result.ops)
+    //     console.log(result.insertedCount)
+    //     console.log(result.insertedId)
     // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Bob',
+    //         age: 44
+    //     }, {
+    //         name: 'Gunthuner',
+    //         age: 81
+    //     }
+    // ], (error,result) => {
+    //     if (error) {
+    //         return console.log('cannot insert many collections')
+    //     }
+    //     console.log(result.insertedCount)
+    //     console.log(result.ops)
+
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'this is an easy task',
+            completed: true
+        },
+        {
+            description: 'this medium task',
+            completed: false
+        }, {
+            description: 'very tricky task',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('cannot add many tasks')
+        }
+
+        console.log(result.ops)
+        console.log(result.insertedCount)
+    })
+
+
 })
