edf8f6d6a5160bad5ec6c0351ad7557c0bee437c
sbp8984 <87753278+sbp8984@users.noreply.github.com>
2021-09-21 20:44:20 +0100

    init2

---
+++ mongodb.js
@@ -0,0 +1,8 @@
+// CRUD create, read, update and delete
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const database = 'task-manager'
+
