bb91cba65498d56f06b7836076dd76520a8db9ae
sbp8984 <87753278+sbp8984@users.noreply.github.com>
2021-09-24 14:45:09 +0100

    completed CR out of CRUD

--- mongodb.js
+++ mongodb.js
@@ -5,38 +5,80 @@
 // const ObjectID = mongodb.ObjectID
 
 // This above can be Simplied into below! Destructuring
-const  { MongoClient , ObjectID } = require('mongodb')
+const { MongoClient, ObjectID } = require('mongodb')
 
 //connection to db
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id)
-
 const task1 = 'Get a haircut'
 const task2 = 'Take a dump'
 
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology:true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         return console.log('unable to connect to db')
     }
 
     const db = client.db(databaseName)
 
+    // db.collection('users').findOne({ _id: new ObjectID("614da5094ecda0262ca2cb6f") }, (error, user) => {
+    //     if (error) {
+    //         return console.log('unable to fetch')
+    //     } 
+
+    //     console.log(user)
+    // })
+
+    // db.collection('users').find( { lastname: 'badlandabad' } ).toArray((error,users) => {
+       
+    //     console.log(users)
+    // })
+
+    // db.collection('users').find( { lastname: 'badlandabad' } ).count((error,count) => {
+       
+    //     console.log(count)
+    // })
+
+    db.collection('tasks').findOne({ _id: new ObjectID("614da15b2b28960d6433ee1b") }, (error, task) => {
+        if (error) {
+            console.log('no task id found')
+        }
+
+        console.log(task)
+    })
+
+    db.collection('tasks').find( { completed: false }).toArray((error, tasks) => {
+        if (error) {
+            console.log('no uncompoleted tasks')
+        }
+
+        console.log(tasks)
+    })
+
+
+
+
+ 
+
     // db.collection('users').insertOne({
-    //     name: 'Shyam',
-    //     age: 30
+    //     firstname: 'Taj mahal ',
+    //     password: 'p13ass5methjemony',
+    //     email: 'vik113rm1@fmai.com',
+    //     lastname: 'badlandabad',
     // }, (error, result) => {
     //     if (error) {
     //         return console.log('unable to insert user')
-    //     } 
+    //     }
 
     //     console.log(result.ops)
     //     console.log(result.insertedCount)
     //     console.log(result.insertedId)
     // })
+
+
+
+
 
     // db.collection('users').insertMany([
     //     {
@@ -53,30 +95,5 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology:t
     //     console.log(result.insertedCount)
     //     console.log(result.ops)
 
-    // })
-
-    // 
-
-
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: task1,
-    //         completed: true
-    //     },
-    //     {
-    //         description: task2,
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('cannot add many tasks')
-    //     }
-
-    //     console.log(result.ops)
-    //     console.log(result.insertedCount)
-    // })
-
-  
-
 })
+
